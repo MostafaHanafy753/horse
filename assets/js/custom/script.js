7728a0464eae8140262d024e8f6add5040397b5b
MostafaHanafy753 <moustafa.hanafy1566@gmail.com>
2023-05-28 01:40:12 +0300

    v3

--- assets/js/custom/script.js
+++ assets/js/custom/script.js
@@ -113,6 +113,23 @@
       $('.month-span').hide();
     }
   });
+
+
+
+  $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+
+
 }(jQuery));
 
 // Get the modal
